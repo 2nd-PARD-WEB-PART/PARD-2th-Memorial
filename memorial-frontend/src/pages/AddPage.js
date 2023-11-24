4450c039f987aae95aa29896da479875e6d6c98e
hjkim0905 <eax9952@gmail.com>
2023-11-25 08:38:59 +0900

    231125 현중 : AddPage.js 완성

--- memorial-frontend/src/pages/AddPage.js
+++ memorial-frontend/src/pages/AddPage.js
@@ -1,4 +1,5 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../contexts/userContext';
 import styled from 'styled-components';
 import axios from 'axios';
@@ -17,6 +18,7 @@ const Div = styled.div`
     text-align: ${(props) => props.textAlign || ''};
     line-height: ${(props) => props.lineHeight || ''};
     padding-top: ${(props) => props.paddingTop || ''};
+    padding-left: ${(props) => props.paddingLeft || ''};
     flex-direction: ${(props) => props.flexDirection || ''};
     margin-bottom: ${(props) => props.marginBottom || ''};
     bottom: ${(props) => props.bottom || ''};
@@ -57,6 +59,8 @@ const Input = styled.input`
     font-size: ${(props) => props.fontSize || ''};
     font-weight: ${(props) => props.fontWeight || ''};
     color: ${(props) => props.color || ''};
+    background: ${(props) => props.background || ''};
+    content: ${(props) => props.content || ''};
 `;
 
 //label styled-component
@@ -203,7 +207,10 @@ const Button = styled.button`
 `;
 
 function AddPage() {
+    const initialImageUrl = '/Group_2239.png';
+    const navigate = useNavigate();
     const [imageSrc, setImageSrc] = useState(null);
+    const [imageUrl, setImageUrl] = useState(initialImageUrl);
     const [previewSrc, setPreviewSrc] = useState(null);
     const [logInData, setLogInData] = useContext(UserContext);
     const [title, setTitle] = useState('');
@@ -262,17 +269,19 @@ function AddPage() {
             // 데이터 전송이 성공하면, 파일 업로드를 시작
             console.log(response.data.id);
             uploadFile(response.data.data.id);
+            navigate('/HomePage');
         } catch (error) {
             console.error(error);
         }
     };
+    const dateInputRef = useRef();
     return (
         <Div width="100%" height="1000px" margin="0 auto">
             <Form width="100%" display="flex" onSubmit={onSubmit}>
                 {/*Column 1*/}
                 <Div width="35%" height="1000px">
                     {/*InnerRow1*/}
-                    <Div width="100%" height="1000px" alignItems="center" display="flex">
+                    <Div width="100%" height="1000px" alignItems="center" display="flex" paddingLeft="50px">
                         <Input
                             display="none"
                             id="file"
@@ -300,9 +309,10 @@ function AddPage() {
                             overflow="hidden"
                         >
                             {!previewSrc && (
-                                <P textAlign="center" margin="0">
-                                    이미지를 등록해주세요
-                                </P>
+                                // <P textAlign="center" margin="0">
+                                //     이미지를 등록해주세요
+                                // </P>
+                                <Img width="40%" height="40%" objectFit="contain" src={imageUrl} alt="사진" />
                             )}
                             {previewSrc && (
                                 <Img width="100%" height="100%" objectFit="cover" src={previewSrc} alt="고인의 사진" />
@@ -314,8 +324,21 @@ function AddPage() {
                 <Div width="65%" height="1000px" display="flex" alignItems="center" justifyContent="center">
                     {/*InnerMainRow*/}
                     <Div width="90%" height="500px" display="block" textAlign="center">
+                        {/*첫 헤더*/}
+                        <Div
+                            width="100%"
+                            height="30px"
+                            textAlign="left"
+                            paddingLeft="45px"
+                            color="#161616"
+                            fontSize="25px"
+                            fontWeight="600"
+                            marginBottom="15px"
+                        >
+                            장례식 제목 작성하기 (15자 이하)
+                        </Div>
                         {/*InnerRow1*/}
-                        <Div width="100%" height="100px" marginBottom="10px">
+                        <Div width="100%" height="60px" marginBottom="10px">
                             <Input
                                 borderRadius="38px"
                                 border="none"
@@ -324,30 +347,59 @@ function AddPage() {
                                 height="100%"
                                 type="text"
                                 maxLength="15"
-                                placeholder="*버리고 싶은 나의 모습과 나의 이름을 작성해주세요. (예. 손톱 무는 홍길동"
-                                fontSize="30px"
+                                placeholder="*버리고 싶은 나의 모습과 나의 이름을 작성해주세요. (예. 손톱 무는 홍길동)"
+                                fontSize="18px"
                                 fontWeight="400"
                                 color="#838383"
                                 value={title}
                                 onChange={(e) => setTitle(e.target.value)}
                             />
                         </Div>
+                        {/*두번째 헤더*/}
+                        <Div
+                            width="100%"
+                            height="30px"
+                            textAlign="left"
+                            paddingLeft="45px"
+                            color="#161616"
+                            fontSize="25px"
+                            fontWeight="600"
+                            marginBottom="15px"
+                        >
+                            장례식 시작일
+                        </Div>
                         {/*InnerRow 2*/}
                         <Div width="100%" height="80px" marginBottom="10px">
                             <Input
+                                ref={dateInputRef}
                                 borderRadius="38px"
                                 border="none"
                                 backgroundColor="#F0F0F0"
                                 width="90%"
                                 height="100%"
-                                type="datetime-local"
-                                placeholder="장례식 시작일"
-                                fontSize="30px"
+                                type="text"
+                                placeholder="*장례식은 3일장으로 치뤄지며 그 이후에는 조문객들에게 댓글을 받을 수 없습니다."
+                                fontSize="18px"
                                 fontWeight="400"
                                 color="#838383"
+                                onFocus={() => (dateInputRef.current.type = 'date')}
+                                onBlur={() => (dateInputRef.current.type = 'text')}
                                 value={startDate}
                                 onChange={(e) => setStartDate(e.target.value)}
                             />
+                        </Div>
+                        {/*세번째 헤더*/}
+                        <Div
+                            width="100%"
+                            height="30px"
+                            textAlign="left"
+                            paddingLeft="45px"
+                            color="#161616"
+                            fontSize="25px"
+                            fontWeight="600"
+                            marginBottom="15px"
+                        >
+                            유언장 작성하기 (200자 이하)
                         </Div>
                         {/*InnerRow 3*/}
                         <Div width="100%" height="200px" marginBottom="10px">
@@ -356,9 +408,9 @@ function AddPage() {
                                 backgroundColor="#F0F0F0"
                                 width="90%"
                                 height="100%"
-                                maxLength="500"
+                                maxLength="200"
                                 placeholder="*내가 버리고 싶은 버릇에 대한 스토리와 찾아와준 조문객들에게 전하고 싶은 말을 작성해주세요."
-                                fontSize="30px"
+                                fontSize="18px"
                                 fontWeight="400"
                                 color="#838383"
                                 value={content}
@@ -378,7 +430,7 @@ function AddPage() {
                                 borderRadius="38px"
                                 border="none"
                                 color="#F0F0F0"
-                                fontSize="30px"
+                                fontSize="20px"
                                 fontWeight="600"
                                 margin="20px 0"
                                 cursor="pointer"
