3e2ee5fec43fbccd7441d5c941d167834fce0ad3
hjkim0905 <eax9952@gmail.com>
2023-11-25 01:54:21 +0900

    231125 현중 : AddPage.js 수정

--- memorial-frontend/src/pages/AddPage.js
+++ memorial-frontend/src/pages/AddPage.js
@@ -1,9 +1,29 @@
+import react from "react";
+import styled from "styled-components";
+
+// div styled-component
+const Div = styled.div`
+  /* 가로, 길이 */
+  width: ${(props) => props.width || "50vw"};
+  height: ${(props) => props.height || "50vh"};
+
+  /* 배치 */
+  display: ${(props) => props.display || "flex"};
+  margin: ${(props) => props.margin || "25vh auto"};
+  justify-content: ${(props) => props.justifyContent || "center"};
+  align-items: ${(props) => props.alignItems || "center"};
+
+  /* 배경색 */
+  background-color: ${(props) => props.backgroundColor || "aqua"};
+  border-radius: ${(props) => props.borderRadius || "30px"};
+`;
+
 function AddPage() {
-    return (
-        <div>
-            <p>AddPage입니다.</p>
-        </div>
-    );
+  return (
+    <div>
+      <p>AddPage입니다.</p>
+    </div>
+  );
 }
 
 export default AddPage;
