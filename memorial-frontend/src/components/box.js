25643951e53d30d3cbe4bf28263bcca6b74ea397
pangil5634 <oksk6681@naver.com>
2023-11-25 01:04:48 +0900

    231125 광일 : 중간 commit (for pull)

--- memorial-frontend/src/components/box.js
+++ memorial-frontend/src/components/box.js
@@ -4,4 +4,14 @@ export const Div = styled.div `
     display: ${props => props.display || "flex"};
     justify-content: ${props => props.justifyContent || "center"};
     align-items: ${props => props.alignItems || "center"};
+    width : ${props => props.width || "100%"};
+    height : ${props => props.height || "100px"};
+    border : ${props => props.border || "1px solid black"};
+    box-sizing : ${props => props.border || "border-box"};
+
+`;
+
+export const Menu = styled.label`
+    margin : ${props => props.margin || "20px"};
+    text-decoration: ${props => props.textDecoration || "none"};
 `;
--- memorial-frontend/src/pages/Layout.js
+++ memorial-frontend/src/pages/Layout.js
@@ -1,14 +1,14 @@
 import {Link} from "react-router-dom";
 import { Outlet } from 'react-router-dom';
-import { Div } from '../components/box';
+import { Div, Menu } from '../components/box';
 
 function Layout() {
     return (
         <div>
-            <Div>
-                <Link to="/">HomePage</Link>
-                <Link to="/AddPage">AddPage</Link>
-                <Link to="/ViewPage">ViewPage</Link>
+            <Div justifyContent = "start" height = "40px">
+                <Link to="/"><Menu>HomePage</Menu></Link>
+                <Link to="/AddPage"><Menu>AddPage</Menu></Link>
+                <Link to="/ViewPage"><Menu>ViewPage</Menu></Link>
             </Div>
 
             <main>
