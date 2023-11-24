82e347814b8f1737da1db4df4d1dd50eec33f6e4
pangil5634 <oksk6681@naver.com>
2023-11-25 02:09:27 +0900

    231125 광일 : 메뉴바 초안 완료

--- memorial-frontend/src/components/box.js
+++ memorial-frontend/src/components/box.js
@@ -4,14 +4,20 @@ export const Div = styled.div `
     display: ${props => props.display || "flex"};
     justify-content: ${props => props.justifyContent || "center"};
     align-items: ${props => props.alignItems || "center"};
-    width : ${props => props.width || "100%"};
+    /* width : ${props => props.width || "100%"}; */
     height : ${props => props.height || "100px"};
     border : ${props => props.border || "1px solid black"};
     box-sizing : ${props => props.border || "border-box"};
+    background-color: ${props => props.backgroundColor || " black"};
+    color: ${props => props.backgroundColor || " white"};
+
 
 `;
 
-export const Menu = styled.label`
+export const Menu = styled.label `
     margin : ${props => props.margin || "20px"};
-    text-decoration: ${props => props.textDecoration || "none"};
+
+    &:hover{
+        color : yellow;
+    }
 `;
--- memorial-frontend/src/pages/Layout.js
+++ memorial-frontend/src/pages/Layout.js
@@ -1,14 +1,31 @@
 import {Link} from "react-router-dom";
-import { Outlet } from 'react-router-dom';
-import { Div, Menu } from '../components/box';
+import {Outlet} from 'react-router-dom';
+import {Div, Menu} from '../components/box';
 
 function Layout() {
+    const linkStyle = {
+        textDecoration: 'none', // 링크의 밑줄을 없애는 스타일
+        color: 'white', // 링크의 기본 색상 사용
+        transition: 'color 0.3s ease',
+    };
+    const hoverStyle = {
+        color: 'blue', // hover 시의 색상
+    };
     return (
         <div>
-            <Div justifyContent = "start" height = "40px">
-                <Link to="/"><Menu>버르장 온라인 장례식</Menu></Link>
-                <Link to="/AddPage"><Menu>장례식 신청하기</Menu></Link>
-                <Link to="/ViewPage"><Menu>남의 버릇 구경가기</Menu></Link>
+            <Div justifyContent="space-between" height="40px" bgColor="black">
+
+                <Link to="/" style={linkStyle} activeStyle={hoverStyle}>
+                    <Menu>천국 말고 홈화면으로 돌아가시기</Menu>
+                </Link>
+                <Div height="40px" >
+                    <Link to="/AddPage" style={linkStyle}>
+                        <Menu>장례식 등록하기</Menu>
+                    </Link>
+                    <Link to="/ViewPage" style={linkStyle}>
+                        <Menu>마이페이지</Menu>
+                    </Link>
+                </Div>
             </Div>
 
             <main>
