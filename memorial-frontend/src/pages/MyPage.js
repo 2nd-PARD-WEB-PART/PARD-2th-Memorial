f2a0b64d6dead8112351050d238149edbf1b0602
hjkim0905 <eax9952@gmail.com>
2023-11-25 09:25:07 +0900

    231125 현중 : MyPage.js 수정

--- memorial-frontend/src/pages/MyPage.js
+++ memorial-frontend/src/pages/MyPage.js
@@ -1,12 +1,8 @@
-import { Div, P } from '../components/box'
-import Grid from '../components/Grid'
+import { Div, P } from '../components/box';
+import Grid from '../components/Grid';
 function MyPage() {
     return (
-        <Div
-            backgroundColor="none"
-            border="none"
-            height="calc(200vh - 40px)"
-            flexDirection="column">
+        <Div backgroundColor="none" border="none" height="calc(200vh - 40px)" flexDirection="column">
             <Div
                 width="100%"
                 backgroundColor="none"
@@ -15,139 +11,13 @@ function MyPage() {
                 flexDirection="column"
                 justifyContent="start"
                 alignItems="top"
-                padding="50px">
-
+                padding="50px"
+            >
                 <P fontSize="30px">마이페이지</P>
-                <P fontSize="15px" margin="20px 0px">버리고자 하는 버릇들이 장례식을 기다리고 있어요.</P>
-                {/* <Div
-                    width="100%"
-                    backgroundColor="none"
-                    border="none"
-                    height="400px"
-                    justifyContent="start"
-                    alignItems="top"
-                    padding="0px"
-                    margin="0% 0% 5% 0%">
-                    <Div
-                        width="20%"
-                        padding="1%"
-                        backgroundColor="none"
-                        height="100%"
-                        border=""
-                        flexDirection="column"
-                        margin="0% 5% 0% 0%">
-                        <img src="" alt="" width="100%" height="100%" padding="0"/>
-                        <P fontSize="20px" margin="20px 0px">담배피는 현중</P>
-
-                        <P fontSize="20px">담배피는 현중</P>
-                    </Div>
-                    <Div
-                        width="20%"
-                        padding="1%"
-                        backgroundColor="none"
-                        height="100%"
-                        border=""
-                        flexDirection="column"
-                        margin="0% 5% 0% 0%">
-                        <img src="" alt="" width="100%" height="100%" padding="0"/>
-                        <P fontSize="20px" margin="20px 0px">담배피는 현중</P>
-
-                        <P fontSize="20px">담배피는 현중</P>
-                    </Div>
-                    <Div
-                        width="20%"
-                        padding="1%"
-                        backgroundColor="none"
-                        height="100%"
-                        border=""
-                        flexDirection="column"
-                        margin="0% 5% 0% 0%">
-                        <img src="" alt="" width="100%" height="100%" padding="0"/>
-                        <P fontSize="20px" margin="20px 0px">담배피는 현중</P>
-
-                        <P fontSize="20px">담배피는 현중</P>
-                    </Div>
-                    <Div
-                        width="20%"
-                        padding="1%"
-                        backgroundColor="none"
-                        height="100%"
-                        border=""
-                        flexDirection="column"
-                        margin="0% 5% 0% 0%">
-                        <img src="" alt="" width="100%" height="100%" padding="0"/>
-                        <P fontSize="20px" margin="20px 0px">담배피는 현중</P>
-
-                        <P fontSize="20px">담배피는 현중</P>
-                    </Div>
-
-                </Div> */}
-                {/* <Div
-                    width="100%"
-                    backgroundColor="none"
-                    border="none"
-                    height="400px"
-                    justifyContent="start"
-                    alignItems="top"
-                    padding="0px"
-                    margin="0% 0% 5% 0%">
-                    <Div
-                        width="20%"
-                        padding="1%"
-                        backgroundColor="none"
-                        height="100%"
-                        border=""
-                        flexDirection="column"
-                        margin="0% 5% 0% 0%">
-                        <img src="" alt="" width="100%" height="100%" padding="0"/>
-                        <P fontSize="20px" margin="20px 0px">담배피는 현중</P>
-
-                        <P fontSize="20px">담배피는 현중</P>
-                    </Div>
-                    <Div
-                        width="20%"
-                        padding="1%"
-                        backgroundColor="none"
-                        height="100%"
-                        border=""
-                        flexDirection="column"
-                        margin="0% 5% 0% 0%">
-                        <img src="" alt="" width="100%" height="100%" padding="0"/>
-                        <P fontSize="20px" margin="20px 0px">담배피는 현중</P>
-
-                        <P fontSize="20px">담배피는 현중</P>
-                    </Div>
-                    <Div
-                        width="20%"
-                        padding="1%"
-                        backgroundColor="none"
-                        height="100%"
-                        border=""
-                        flexDirection="column"
-                        margin="0% 5% 0% 0%">
-                        <img src="" alt="" width="100%" height="100%" padding="0"/>
-                        <P fontSize="20px" margin="20px 0px">담배피는 현중</P>
-
-                        <P fontSize="20px">담배피는 현중</P>
-                    </Div>
-                    <Div
-                        width="20%"
-                        padding="1%"
-                        backgroundColor="none"
-                        height="100%"
-                        border=""
-                        flexDirection="column"
-                        margin="0% 5% 0% 0%">
-                        <img src="" alt="" width="100%" height="100%" padding="0"/>
-                        <P fontSize="20px" margin="20px 0px">담배피는 현중</P>
-
-                        <P fontSize="20px">담배피는 현중</P>
-                    </Div>
-
-                </Div> */}
-                <Grid>
-
-                </Grid>
+                <P fontSize="15px" margin="20px 0px">
+                    버리고자 하는 버릇들이 장례식을 기다리고 있어요.
+                </P>
+                <Grid></Grid>
             </Div>
         </Div>
     );
